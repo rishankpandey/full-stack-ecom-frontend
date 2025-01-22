bda08fe4091470e70d1dee47d30379dc6df8d9a1
rishankpandey <pandey.rishank2506@gmail.com>
2025-01-22 08:10:20 +0530

    index

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     ecomApiUrl: "https://full-stack-ecomm.onrender.com/api",
+    //ecomApiUrl: "http://localhost:8443/api",
     stripePublishableKey: "pk_test_51QfdjSBGinLYIPfQwXsKHNhs2iioxMqhsZlY8a41GfEovluujLEzMR7FkmF25lOsDHIbEzQIPaDMN9E2Q4QH0afE00MtBjki1p"
 };
